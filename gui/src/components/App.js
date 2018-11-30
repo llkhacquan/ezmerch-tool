581e506052abd9236570c58cd40433b9e510f88e
Kien.NK <kien.nk@teko.vn>
2018-11-30 15:56:51 +0700

    demo save file

--- gui/src/components/App.js
+++ gui/src/components/App.js
@@ -91,7 +91,24 @@ class App extends Component {
     [11.72, 14.99, 11.72, 14.99],
   ];
 
-  handleSave = () => {};
+  handleSave = () => {
+    var saveData = (function() {
+      var a = document.createElement('a');
+      document.body.appendChild(a);
+      a.style = 'display: none';
+      return function(data, fileName) {
+        var json = JSON.stringify(data),
+          blob = new Blob([json], { type: 'octet/stream' }),
+          url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      };
+    })();
+
+    saveData(this.state, 'form.json');
+  };
 
   handleChangeProductType = (productType) => {
     this.setState({
